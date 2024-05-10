17928b85a797b3eb22bb0bbc9013957b7e11fc07
TheKingJoker18 <153758550+TheKingJoker18@users.noreply.github.com>
2024-05-10 06:44:47 +0300

    Update compilate.bat

--- development/compilate.bat
+++ development/compilate.bat
@@ -17,7 +17,7 @@ xcopy "%src%\controller\*.java" "%temp%" /e /y
 rem Compilation des classes dans [temp] vers [bin]
 javac --release 8 -d "%bin%" -cp "%lib%\*" "%temp%\*.java"
 
-rem Ajout de l'application dans une archive war
+rem Ajout de l'application dans une archive jar
 jar cvf "%nomFramework%.jar" -C "%bin%" .
 
 rem Deploiement de l'application vers Tomcat
