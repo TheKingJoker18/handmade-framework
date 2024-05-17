54982af1cacdb920a59ae0fe6d52cb3640e3bc90
TheKingJoker18 <153758550+TheKingJoker18@users.noreply.github.com>
2024-05-17 07:10:20 +0300

    AnnotationController.java, ControllerScanner.java, FrontController.java and compilate.bat upload

---
+++ development/src/compilate.bat
@@ -0,0 +1,31 @@
+@echo all
+
+rem Creation des variables
+set "temp=.\temp"
+set "src=.\src"
+set "lib=..\test\lib"
+set "bin=.\bin"
+set "destination=..\test\lib"
+set "nomFramework=handmade_framework"
+
+rem Creation du repertoire [temp]
+mkdir "%temp%"
+
+rem Copie des fichiers Java du répertoire [src] vers [temp]
+xcopy "%src%\controller\*.java" "%temp%" /e /y
+xcopy "%src%\utils\*.java" "%temp%" /e /y
+
+rem Compilation des classes dans [temp] vers [bin]
+javac --release 8 -d "%bin%" -cp "%lib%\*" "%temp%\*.java"
+rem Si "servlet-api.jar" est configurer dans CLASSPATH, enlever "-cp "%lib%\*" "
+
+rem Ajout de l'application dans une archive jar
+jar cvf "%nomFramework%.jar" -C "%bin%" .
+
+rem Envoie de l'archive jar vers le [lib] du test
+xcopy ".\%nomFramework%.jar" "%destination%" /y
+
+rem Supression de [temp]
+rmdir /s /q "%temp%"
+
+pause
--- development/src/controller/FrontController.java
+++ development/src/controller/FrontController.java
@@ -1,57 +1,72 @@
 package controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.List;
 
+import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.servlet.ServletContext;
+
+import utils.*;
 
 public class FrontController extends HttpServlet {
 
-    public void processRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
+    private String controllerPackage;
+    private ControllerScanner scanner;
+    private List<Class<?>> controllers;
 
+    @Override
+    public void init(ServletConfig config) 
+        throws ServletException 
+    {
         try {
-            out.println("<h1>Code 200</h1>");
-            out.println("<p>Vous avez entre avec succes dans ce site :) </p>");
-            out.println("<p>Page URL: <b>" + req.getRequestURL() + "</b> </p>");
+            super.init(config);
+
+            ServletContext context = config.getServletContext();
+            controllerPackage = context.getInitParameter("base_package");
+
+            this.scanner = new ControllerScanner();
+            this.controllers = scanner.findControllers(controllerPackage);
+        } 
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException 
+    { 
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException 
+    { 
+        try {
+            PrintWriter out = response.getWriter();
+            String url = request.getRequestURI(); 
             
-        } catch (Exception e) {
-            out.println("<h1>Code 400</h1>");
-            out.println("<p>An error has occurred: " + e.getMessage() + "</p>");
-            e.printStackTrace();
-        }
+            out.println("<h1>" + " Hello World " + "</h1>");
+            out.println("<h2>" + " You are here now: " + "</h2>");
+            out.println("<h3> URL: " + url + " </h3>");
 
-    }
-    
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-
-        try {
-            processRequest(req, res);
-            out.println("<h2>Request method: GET</h2>");
-
-        } catch (Exception e) {
-            out.println("<h1>Code 400</h1>");
-            out.println("<p>An error has occurred: " + e.getMessage() + "</p>");
+            for (Class<?> controller : this.controllers) {
+                out.println("Found controller: " + controller.getName() + "<br>");
+            }
+        } 
+        
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-
-        try {
-            processRequest(req, res);
-            out.println("<h2>Request method: POST</h2>");
-            
-        } catch (Exception e) {
-            out.println("<h1>Code 400</h1>");
-            out.println("<p>An error has occurred: " + e.getMessage() + "</p>");
-            e.printStackTrace();
-        }
-    }
-
 }
