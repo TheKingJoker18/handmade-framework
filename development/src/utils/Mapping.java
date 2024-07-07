e613355c4ffd43676645615b6e6d16d75d2bd6d8
TheKingJoker18 <153758550+TheKingJoker18@users.noreply.github.com>
2024-07-07 13:01:38 +0300

    Sprint 8: Adding a new class MySession.java
    - Modifying Reflect.java by adding the following method: checkFieldByType (to check if the object have the specified Type in its fields), getFieldValueByType, invokeGetterMethod and invokeControllerConstructor
    - Modifying Mapping.java by modifying some of its method like invokeMethod, configParam, and setSimpleParam

--- development/src/utils/Mapping.java
+++ development/src/utils/Mapping.java
@@ -51,27 +51,34 @@ public class Mapping {
                 '}';
     }
 
-    public void setSimpleParam(int i, Parameter[] parameters, Object[] values, HttpServletRequest request) throws NullPointerException {
+    public void setSimpleParam(Object controller, int i, Parameter[] parameters, Object[] values, HttpServletRequest request) throws NullPointerException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, SecurityException {
         Param param = parameters[i].getAnnotation(Param.class);
         String paramName = null;
-        if (param == null)                                      paramName = parameters[i].getName();
-        else                                                    paramName = param.name();
+        if (parameters[i].getType() != MySession.class && param == null)    throw new IllegalArgumentException("There is no @Param annotation on the parameter (parameter place : no." + (i + 1) + " ) ");
+        else if (parameters[i].getType() != MySession.class)                paramName = param.name();
         String paramValue = request.getParameter(paramName);
-        if (paramValue == null) {
+        if (parameters[i].getType() != MySession.class && paramValue == null) {
             throw new NullPointerException("There is an undefined parameter (parameter name : " + paramName + ")");
         } else {
-            if (parameters[i].getType() == int.class)           values[i] = Integer.valueOf(paramValue);
-            else if (parameters[i].getType() == double.class)   values[i] = Double.valueOf(paramValue);
-            else if (parameters[i].getType() == float.class)    values[i] = Float.valueOf(paramValue);
-            else                                                values[i] = (String) paramValue;
+            if (parameters[i].getType() == int.class)                       values[i] = Integer.valueOf(paramValue);
+            else if (parameters[i].getType() == double.class)               values[i] = Double.valueOf(paramValue);
+            else if (parameters[i].getType() == float.class)                values[i] = Float.valueOf(paramValue);
+            else if (parameters[i].getType() == MySession.class) {
+                if (Reflect.getFieldValueByType(controller, MySession.class) != null) {
+                    values[i] = (MySession) Reflect.getFieldValueByType(controller, MySession.class);
+                } else {
+                    values[i] = new MySession(request.getSession());
+                }
+            }
+            else                                                            values[i] = (String) paramValue;
         }
     }
 
     public void setModelParam(int i, Parameter[] parameters, Object[] values, HttpServletRequest request) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
         ModelAttribute attribute = parameters[i].getAnnotation(ModelAttribute.class);
         String modelName = null;
-        if (attribute == null)                                  modelName = parameters[i].getName();
-        else                                                    modelName = attribute.name();
+        if (attribute == null)                                              throw new IllegalArgumentException("There is no @ModelAttribute annotation on the model parmeter (parameter place : no." + (i + 1) + " )");
+        else                                                                modelName = attribute.name();
         Object model = Reflect.invokeEmptyConstructor(parameters[i].getType().getName());
         String[] fieldNames = Reflect.getFieldsNames(model);
         Field[] fields = model.getClass().getDeclaredFields();
@@ -80,20 +87,20 @@ public class Mapping {
             String fieldValue = request.getParameter(inputName);
             if (fieldValue == null) {
                 throw new NullPointerException("There is an undefined field (field name : " + inputName + ")");
-            } else {
-                if (fields[j].getType() == int.class)           Reflect.invokeSetterMethod(model, fieldNames[j], int.class, Integer.valueOf(fieldValue));
-                else if (fields[j].getType() == double.class)   Reflect.invokeSetterMethod(model, fieldNames[j], double.class, Double.valueOf(fieldValue));
-                else if (fields[j].getType() == float.class)    Reflect.invokeSetterMethod(model, fieldNames[j], float.class, Float.valueOf(fieldValue));
-                else                                            Reflect.invokeSetterMethod(model, fieldNames[j], String.class, fieldValue);
+            } else { 
+                if (fields[j].getType() == int.class)                       Reflect.invokeSetterMethod(model, fieldNames[j], int.class, Integer.valueOf(fieldValue));
+                else if (fields[j].getType() == double.class)               Reflect.invokeSetterMethod(model, fieldNames[j], double.class, Double.valueOf(fieldValue));
+                else if (fields[j].getType() == float.class)                Reflect.invokeSetterMethod(model, fieldNames[j], float.class, Float.valueOf(fieldValue));
+                else                                                        Reflect.invokeSetterMethod(model, fieldNames[j], String.class, fieldValue);
             }
         }
         values[i] = model;
     }
 
-    public void configParam(Parameter[] parameters, Object[] values, HttpServletRequest request) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
+    public void configParam(Object controller, Parameter[] parameters, Object[] values, HttpServletRequest request) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
         for (int i = 0; i < parameters.length; i++) {
-            if (parameters[i].getType() == int.class || parameters[i].getType() == double.class || parameters[i].getType() == String.class) {
-                setSimpleParam(i, parameters, values, request);
+            if (parameters[i].getType() == int.class || parameters[i].getType() == double.class || parameters[i].getType() == String.class || parameters[i].getType() == MySession.class) {
+                setSimpleParam(controller, i, parameters, values, request);
             } else {
                 setModelParam(i, parameters, values, request);
             }
@@ -101,14 +108,15 @@ public class Mapping {
     }
 
     public Object invokeMethod(HttpServletRequest request) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
-        Method method = Reflect.getMethodByName(Reflect.invokeEmptyConstructor(this.getClassName()), this.getMethodName());
+        Object controller = Reflect.invokeControllerConstructor(this.getClassName(), request);
+        Method method = Reflect.getMethodByName(controller, this.getMethodName());
         Parameter[] parameters = method.getParameters();
         if (parameters.length > 0) {
             Object[] values = new Object[parameters.length];
-            configParam(parameters, values, request);
-            return Reflect.invokeMethod(Reflect.invokeEmptyConstructor(this.getClassName()), this.getMethodName(), values);
+            configParam(controller, parameters, values, request);
+            return Reflect.invokeMethod(controller, this.getMethodName(), values);
         }
-        return Reflect.invokeMethod(Reflect.invokeEmptyConstructor(this.getClassName()), this.getMethodName(), null);
+        return Reflect.invokeMethod(controller, this.getMethodName(), null);
     }
 
     public String execute(HttpServletRequest request, HttpServletResponse response) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException, ServletException, IOException {
